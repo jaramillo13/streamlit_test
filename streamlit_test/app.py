79fb2431c9a9e616e4c6592de3fe6e648962d64b
jaramillo <jaramillo@Juans-MacBook-Pro.local>
2026-02-16 09:47:16 -0600

    second

--- streamlit_test/app.py
+++ streamlit_test/app.py
@@ -1,16 +1,5 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2867
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red203\green35\blue57;\red0\green0\blue0;\red0\green0\blue0;
-\red6\green33\blue79;}
-{\*\expandedcolortbl;;\cssrgb\c84314\c22745\c28627;\cssrgb\c0\c0\c0\c7843;\cssrgb\c0\c0\c0\c83137;
-\cssrgb\c1176\c18431\c38431;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+import streamlit as st
 
-\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 import\cf4 \strokec4  streamlit \cf2 \strokec2 as\cf4 \strokec4  st\
-\
-st.header(\cf5 \strokec5 'Lanzar una moneda'\cf4 \strokec4 )\
-\
-st.write(\cf5 \strokec5 'Esta aplicaci\'f3n a\'fan no es funcional. En construcci\'f3n.'\cf4 \strokec4 )}
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
